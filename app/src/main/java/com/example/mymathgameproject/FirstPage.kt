0b3a43b0579c2da1a380b7cbfb1cff9e7fd6db74
AsenTerziyski <asenterziyski@gmail.com>
2024-01-06 14:00:42 +0200

    add first page preview

--- app/src/main/java/com/example/mymathgameproject/FirstPage.kt
+++ app/src/main/java/com/example/mymathgameproject/FirstPage.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -17,12 +22,23 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FirstPage() {
+fun FirstPage(navController: NavController) {
+
+    val systemUIController = rememberSystemUiController()
+    systemUIController.setSystemBarsColor(
+        color = colorResource(id = R.color.green)
+    )
+
+
 
     Scaffold(
         topBar = {
@@ -30,7 +46,7 @@ fun FirstPage() {
                 title = {
                     Text("Math Game", fontSize = 20.sp)
                 },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
+                colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = colorResource(id = R.color.green),
                     titleContentColor = Color.White
                 )
@@ -49,6 +65,74 @@ fun FirstPage() {
                 verticalArrangement = Arrangement.SpaceEvenly
             ) {
 
+                Button(
+                    onClick = {
+                        navController.navigate("SecondPage/add")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.green)
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier.size(250.dp, 100.dp)
+                ) {
+                    Text(
+                        text = "Addition",
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        navController.navigate("SecondPage/sub")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.green)
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier.size(250.dp, 100.dp)
+                ) {
+                    Text(
+                        text = "Subtraction",
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        navController.navigate("SecondPage/multi")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.green)
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier.size(250.dp, 100.dp)
+                ) {
+                    Text(
+                        text = "Multiplication",
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        navController.navigate("SecondPage/div")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.green)
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier.size(250.dp, 100.dp)
+                ) {
+                    Text(
+                        text = "Division",
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
+                }
+
             }
         }
     )
@@ -58,5 +142,6 @@ fun FirstPage() {
 @Preview
 @Composable
 fun FirstPagePreview() {
-    FirstPage()
+    val myContext = LocalContext.current
+    FirstPage(NavController(myContext))
 }
