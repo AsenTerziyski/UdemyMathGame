1e4718e09e2d613506353dc67c6855b61593cf0c
AsenTerziyski <asenterziyski@gmail.com>
2024-01-08 08:22:37 +0200

    second page - add texts

--- app/src/main/java/com/example/mymathgameproject/pages/SecondPage.kt
+++ app/src/main/java/com/example/mymathgameproject/pages/SecondPage.kt
@@ -1,8 +1,12 @@
 package com.example.mymathgameproject.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,6 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
@@ -21,6 +29,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.mymathgameproject.R
@@ -30,6 +39,20 @@ import com.example.mymathgameproject.SystemBarHelper
 @Composable
 fun SecondPage(navController: NavController, category: String) {
     SystemBarHelper.setColor()
+
+    val life = remember {
+        mutableStateOf(3)
+    }
+
+    val score = remember {
+        mutableStateOf(0)
+    }
+
+    val remainingTime = remember {
+        mutableStateOf("30")
+    }
+
+
 
     Scaffold(
         topBar = {
@@ -74,7 +97,23 @@ fun SecondPage(navController: NavController, category: String) {
                     )
                     .padding(it)
             ) {
-
+                Spacer(modifier = Modifier.size(30.dp))
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Life: ", fontSize = 16.sp, color = Color.White)
+                    Text(text = life.value.toString(), fontSize = 16.sp, color = Color.White)
+                    Text(text = "Score: ", fontSize = 16.sp, color = Color.White)
+                    Text(text = score.value.toString(), fontSize = 16.sp, color = Color.White)
+                    Text(text = "Remaining Time: ", fontSize = 16.sp, color = Color.White)
+                    Text(
+                        text = remainingTime.value,
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
             }
         }
     )
